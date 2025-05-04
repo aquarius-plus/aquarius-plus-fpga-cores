6b338e36015150a8f5cf46b3ee2198e396ec054d
Frank van den Hoef <fvdhoef@icloud.com>
2025-05-04 15:58:01 +0200

    .

--- aq32/software/basic/run.sh
+++ aq32/software/basic/run.sh
@@ -3,4 +3,3 @@ set -e
 ninja -C build
 cp build/basic.aq32 ~/Work/aquarius-plus/EndUser/sdcard/cores/aq32/
 ~/Work/aquarius-plus/System/emulator/build/aqplus-emu -t ' run aq32.core\n'
-
--- aq32/software/basic/upload.sh
+++ aq32/software/basic/upload.sh
@@ -3,4 +3,4 @@ set -e
 ninja -C build
 curl -X DELETE http://aqplus-minivz/cores/aq32/basic.aq32
 curl -X PUT -T build/basic.aq32 http://aqplus-minivz/cores/aq32/basic.aq32
-# printf '\x1E' | curl --data-binary @- http://aqplus-minivz/keyboard
+printf '\x1Ebasic\n' | curl --data-binary @- http://aqplus-minivz/keyboard
