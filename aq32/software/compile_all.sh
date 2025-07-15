8fa3af1d58cec78704ee3f5be798fd3672c20864
Frank van den Hoef <fvdhoef@icloud.com>
2025-07-15 12:43:25 +0200

    Simplified CMakeLists

--- aq32/software/compile_all.sh
+++ aq32/software/compile_all.sh
@@ -9,5 +9,6 @@ for i in $PROJECTS; do
     ninja -C $i/build
 
     cp $i/build/$i.aq32 ../../../../EndUser/sdcard/cores/aq32/
-
 done
+
+cp -f basic/help/content/basic.hlp ../../../../EndUser/sdcard/cores/aq32/
